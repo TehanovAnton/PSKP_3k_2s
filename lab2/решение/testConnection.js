acd734d4b71d973523ea4552a916c6914dac0498
antony <tehanovanton@gmail.com>
2022-02-15 11:56:19 +0300

    return html

---
+++ lab2/решение/testConnection.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+    'pskp', 'Anton', 'ewqqwe',
+    { 
+        host: 'localhost', dialect: 'mssql', 
+        pool: { max:5, min:0, acquire:30000, idle:10000 }
+    }
+);
+
+let testConnection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+testConnection()
